7975af09e49408e1d5b65e3bbfdb6d429450eb80
Meet Patel <meetpatel@bytestechnolab.com>
2023-08-28 17:49:46 +0530

    sss

--- meet.ts
+++ meet.ts
@@ -2,11 +2,8 @@ const me: object = {
     name: 'Meet',
     lname: 'Patel',
     age: 28,
-<<<<<<< HEAD
     one: 1,
     two: 2,
     three: 3,
-=======
     nationality: 'Indian'
->>>>>>> the-first
 }
